1edc99e8e964463611413f036fb7a01d3883eba8
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-09-20 17:17:07 -0500

    modifies created object in various ways

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "phil",
+    streetAddress: "101 street way",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeObject = {...employee};
+    newEmployeeObject[key] = value;
+    return newEmployeeObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
